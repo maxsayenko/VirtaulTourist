ced86e8fa57a3df3e43ba219a5a6db14d80f908a
Max Saienko <msaienko@ancestry.com>
2016-06-18 00:26:41 -0700

    Adding Map View

---
+++ VirtualTourist/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  VirtualTourist
+//
+//  Created by Max Saienko on 6/18/16.
+//  Copyright © 2016 Max Saienko. All rights reserved.
+//
+
+import MapKit
+import UIKit
+
+class MapViewController: UIViewController {
+    @IBOutlet var map: MKMapView!
+    
+    override func viewDidLoad() {
+        
+    }
+}
