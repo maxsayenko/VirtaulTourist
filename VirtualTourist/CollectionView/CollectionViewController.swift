ea60dbffcef565cf46d2f96ad94773bc0040119e
Max Saienko <msaienko@ancestry.com>
2016-08-16 21:36:38 -0700

    removing photos on NewCollection click

--- VirtualTourist/CollectionView/CollectionViewController.swift
+++ VirtualTourist/CollectionView/CollectionViewController.swift
@@ -15,7 +15,6 @@ import CoreData
 
 class CollectionViewController: UIViewController {
     var pin:Pin?
-    var collectionImages:[Photo] = []
     var picsToDelete:Set<NSIndexPath> = Set<NSIndexPath>()
     var currentPageIndex = 1
     var totalPagesForThisCollection: Int?
@@ -51,75 +50,13 @@ class CollectionViewController: UIViewController {
     @IBOutlet var newCollectionBtn: UIButton!
     @IBOutlet var deleteBtn: UIButton!
     
-    @IBAction func newCollectionTouch(sender: UIButton) {
-        if let pagesCount = totalPagesForThisCollection {
-            guard currentPageIndex < pagesCount else {
-                debugPrint("No more pages. Total Pages count = \(totalPagesForThisCollection). Current page = \(currentPageIndex)")
-                return
-            }
-        }
-        
-        if let pin = pin {
-            FlickrService.GetImages(pin.annotation, page: ++currentPageIndex).then { result -> Void in
-                self.receiveNewImages(result.pagesCount, photoInfos: result.photoInfos)
-                }.error { error in
-                    debugPrint("Error while fetching new collection photos data: \(error)")
-            }
-        }
-    }
-    
-    @IBAction func deleteTouch(sender: UIButton) {
-        //let sectionInfo = fetchedResultsController.sections![0]
-        for (indexPath) in picsToDelete.sort({ $0.row > $1.row }) {
-            //collectionImages.removeAtIndex(indexPath.row)
-            //sectionInfo.objects?.removeAtIndex(indexPath.row)
-            let item = fetchedResultsController.objectAtIndexPath(indexPath) as! NSManagedObject
-            sharedContext.deleteObject(item)
-            //            self.managedObjectContext.deleteObject(item)
-        }
-        //collection.deleteItemsAtIndexPaths(Array(picsToDelete))
-        picsToDelete.removeAll()
-        
-        updateButtonsVisibility()
-    }
-    
-    func receiveNewImages(pagesCount: Int, photoInfos: [Photo]) {
-        self.totalPagesForThisCollection = pagesCount
-        debugPrint("total pages = \(self.totalPagesForThisCollection)")
-        
-        // Handling 0 or 1 page
-        if (self.totalPagesForThisCollection < 2) {
-            self.newCollectionBtn.enabled = false
-        }
-        
-        // Handling 0 images scenario
-        if (photoInfos.isEmpty) {
-            self.messageLbl.hidden = false
-        }
-        
-        //self.collectionImages = result.photoInfos
-        
-        for photoInfo in photoInfos {
-            photoInfo.pin = pin
-        }
-        
-        //pin.photos = result.photoInfos
-        //self.pin?.photos = NSSet(array: result.photoInfos)
-        
-        // CoreData - Save the context
-        self.saveContext()
-        
-        // Update the collection on the main thread
-        dispatch_async(dispatch_get_main_queue()) {
-            self.collection.reloadData()
-        }
-    }
-    
     override func viewDidLoad() {
         debugPrint("View did load")
+
+        // UI logic
         self.messageLbl.hidden = true
         self.newCollectionBtn.enabled = true
-        
+
         let layout = collection.collectionViewLayout as! UICollectionViewFlowLayout
         let totalSectionsInsets = layout.sectionInset.left + layout.sectionInset.right
         let bounds = UIScreen.mainScreen().bounds
@@ -127,6 +64,7 @@ class CollectionViewController: UIViewController {
         layout.itemSize = CGSizeMake(width, width)
         
         if let pin = pin {
+            // Dropping pin on a small map
             map.addAnnotation(pin.annotation)
             let span = MKCoordinateSpanMake(0.075, 0.075)
             let region = MKCoordinateRegion(center: pin.annotation.coordinate, span: span)
@@ -154,6 +92,73 @@ class CollectionViewController: UIViewController {
         }
     }
     
+    @IBAction func newCollectionTouch(sender: UIButton) {
+        debugPrint("New Collection")
+        if let pagesCount = totalPagesForThisCollection {
+            guard currentPageIndex < pagesCount else {
+                debugPrint("No more pages. Total Pages count = \(totalPagesForThisCollection). Current page = \(currentPageIndex)")
+                return
+            }
+        }
+        
+        if let pin = pin {
+            FlickrService.GetImages(pin.annotation, page: ++currentPageIndex).then { result -> Void in
+                debugPrint("Deleting \(self.fetchedResultsController.fetchedObjects?.count) objects")
+                for (photoObj) in (self.fetchedResultsController.fetchedObjects)! {
+                    self.sharedContext.deleteObject(photoObj as! NSManagedObject)
+                }
+                
+                self.receiveNewImages(result.pagesCount, photoInfos: result.photoInfos)
+                }.error { error in
+                    debugPrint("Error while fetching new collection photos data: \(error)")
+            }
+        }
+    }
+    
+    @IBAction func deleteTouch(sender: UIButton) {
+        for (indexPath) in picsToDelete.sort({ $0.row > $1.row }) {
+            let item = fetchedResultsController.objectAtIndexPath(indexPath) as! NSManagedObject
+            sharedContext.deleteObject(item)
+        }
+        picsToDelete.removeAll()
+        updateButtonsVisibility()
+                debugPrint("!!!!!! fetched objects count = \(fetchedResultsController.fetchedObjects?.count)")
+        // CoreData - saving the deleted images. Also it will allow us to clean up the all pins once new collection is requested
+        saveContext()
+    }
+    
+    func receiveNewImages(pagesCount: Int, photoInfos: [Photo]) {
+        self.totalPagesForThisCollection = pagesCount
+        debugPrint("total pages = \(self.totalPagesForThisCollection)")
+        
+        // Handling 0 or 1 page
+        if (self.totalPagesForThisCollection < 2) {
+            self.newCollectionBtn.enabled = false
+        }
+        
+        // Handling 0 images scenario
+        if (photoInfos.isEmpty) {
+            self.messageLbl.hidden = false
+        }
+        
+        for photoInfo in photoInfos {
+            photoInfo.pin = pin
+        }
+        
+        //pin.photos = result.photoInfos
+        //self.pin?.photos = NSSet(array: result.photoInfos)
+        
+        // CoreData - Save the context
+        self.saveContext()
+        
+        // Update the collection on the main thread
+        dispatch_async(dispatch_get_main_queue()) {
+            self.collection.reloadData()
+        }
+    }
+    
+
+    
     override func viewWillDisappear(animated: Bool) {
         saveContext()
         super.viewWillDisappear(animated)
@@ -178,7 +183,6 @@ class CollectionViewController: UIViewController {
         blockOperations.removeAll(keepCapacity: false)
     }
 }
-
 
 
 
@@ -261,59 +265,56 @@ extension CollectionViewController: NSFetchedResultsControllerDelegate {
         //tableView.beginUpdates()
     }
     
-    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
-        debugPrint("didChangeSection \(type)")
-        switch type {
-        case .Insert:
-            debugPrint("INSERT")
-            //tableView.insertSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
-            
-        case .Delete:
-            debugPrint("Delete")
-            //tableView.deleteSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
-            
-        default:
-            return
-        }
-    }
+//    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+//        debugPrint("=================      didChangeSection \(type)")
+//        switch type {
+//        case .Insert:
+//            debugPrint("INSERT")
+//            //tableView.insertSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+//            
+//        case .Delete:
+//            debugPrint("Delete")
+//            //tableView.deleteSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+//            
+//        default:
+//            return
+//        }
+//    }
     
     func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
-        debugPrint("didChangeObject \(type.rawValue)")
         switch type {
         case .Insert:
             debugPrint("INSERT")
-            //tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
             blockOperations.append(
                 NSBlockOperation(block: { [weak self] in
-                    debugPrint("inserting")
                     self!.collection!.insertItemsAtIndexPaths([newIndexPath!])
                     })
             )
         case .Delete:
             debugPrint("Delete")
-            //            collectionView!.deleteSections(NSIndexSet(index: sectionIndex))
-            
             blockOperations.append(
                 NSBlockOperation(block: { [weak self] in
                     self!.collection.deleteItemsAtIndexPaths([indexPath!])
                     })
             )
-            //tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
         case .Move:
             debugPrint("Move")
-            //tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
-            //tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+            blockOperations.append(
+                NSBlockOperation(block: { [weak self] in
+                    self!.collection.moveItemAtIndexPath(indexPath!, toIndexPath: newIndexPath!)
+                    })
+            )
         case .Update:
-            debugPrint("Update")
-            //            let cell = tableView.cellForRowAtIndexPath(indexPath!) as! ActorTableViewCell
-            //            let movie = self.fetchedResultsController.objectAtIndexPath(indexPath!) as! Movie
-            //            configureCell(cell, movie: movie)
+            blockOperations.append(
+                NSBlockOperation(block: { [weak self] in
+                    self!.collection.reloadItemsAtIndexPaths([indexPath!])
+                    })
+            )
         }
         
     }
     
     func controllerDidChangeContent(controller: NSFetchedResultsController) {
-        print("MovieListViewController - controllerDidChangeContent")
         collection!.performBatchUpdates({ () -> Void in
             for operation: NSBlockOperation in self.blockOperations {
                 operation.start()
@@ -321,6 +322,5 @@ extension CollectionViewController: NSFetchedResultsControllerDelegate {
             }, completion: { (finished) -> Void in
                 self.blockOperations.removeAll(keepCapacity: false)
         })
-        //tableView.endUpdates()
     }
 }
